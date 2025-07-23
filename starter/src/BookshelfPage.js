a0b58cdd5b5d579fb3c2efb191a864da0602941b
Andy Mahaffey <182353@carmax.com>
2025-07-23 07:15:30 -0400

    Add loading indicator to bookshelf page

--- starter/src/BookshelfPage.js
+++ starter/src/BookshelfPage.js
@@ -1,42 +1,42 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import DotLoader from "react-spinners/DotLoader";
 import { getAll, update } from "./BooksAPI";
 import BookList from "./BookList";
 
 function BookshelfPage() {
-  const [bookshelves, setBookshelves] = useState(null);
+  const [bookshelves, setBookshelves] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-      loadBookshelves();
+    setLoading(true);
+    loadBookshelves().finally(() => setLoading(false));
   }, []);
 
   const loadBookshelves = () => {
-    getAll().then((books) => {
-      setBookshelves({
-        currentlyReading: {
+    return getAll().then((books) => {
+      setBookshelves([
+        {
           title: "Currently Reading",
           books: books.filter((book) => book.shelf === "currentlyReading"),
         },
-        wantToRead: {
+        {
           title: "Want to Read",
           books: books.filter((book) => book.shelf === "wantToRead"),
         },
-        read: {
+        {
           title: "Read",
           books: books.filter((book) => book.shelf === "read"),
-        },
-        all: {
-          title: "All Books",
-          books: books,
         }
-      });
+      ]);
     });
   }
   
   const bookshelfChanged = (shelf, book) => {
+    setLoading(true);
     update(book, shelf).then(() => {
-      loadBookshelves();
-    });
+      return loadBookshelves();
+    }).finally(() => setLoading(false));
   }
 
   return (
@@ -45,10 +45,11 @@ function BookshelfPage() {
             <div className="list-books-title">
             <h1>MyReads</h1>
             </div>
+            {loading && <div className="loading-container"><DotLoader size={30} /></div>}
             <div className="list-books-content">
-                {bookshelves?.currentlyReading && <BookList bookshelf={bookshelves.currentlyReading} bookshelfChanged={bookshelfChanged} />}
-                {bookshelves?.wantToRead && <BookList bookshelf={bookshelves.wantToRead} bookshelfChanged={bookshelfChanged} />}
-                {bookshelves?.read && <BookList bookshelf={bookshelves.read} bookshelfChanged={bookshelfChanged} />}
+              {bookshelves.map((bs) => (
+                <BookList key={bs.title} bookshelf={bs} bookshelfChanged={bookshelfChanged} />
+              ))}              
             </div>
             <div className="open-search">
                 <Link to="/search" />
